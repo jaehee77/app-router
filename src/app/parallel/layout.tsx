c234f4dcd03a2c2509d1aef872269e909d11ad15
KimJaeHee <jaiyah@naver.com>
2025-04-07 17:55:31 +0900

    WIP parellel router

--- src/app/parallel/layout.tsx
+++ src/app/parallel/layout.tsx
@@ -3,6 +3,17 @@ import { ReactNode } from 'react';
 // Slot(슬롯) : 병렬로 렌더링 될 페이지 컴포넌트를 보관하는 폴더
 // 예시 : @sidebar/
 
-export default function Layout({ children }: { children: ReactNode }) {
-  return <div>{children}</div>;
+export default function Layout({
+  children,
+  sidebar,
+}: {
+  children: ReactNode;
+  sidebar: ReactNode;
+}) {
+  return (
+    <div>
+      <div>{sidebar}</div>
+      <div>{children}</div>
+    </div>
+  );
 }
